7436ef6808858774a3a4b883be25b21c065f84c3
DevDawn <cobhamdawn00@gmail.com>
2025-03-16 01:57:02 +0100

    Date Invitation Removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import BirthdayWish from './components/BirthdayWish';
 import ImageSlider from './components/ImageSlider';
-import DateInvitation from './components/DateInvitation';
+// import DateInvitation from './components/DateInvitation';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-4">
       <BirthdayWish />
       <ImageSlider />
-      <DateInvitation />
+      {/* <DateInvitation /> */}
     </div>
   );
 }
